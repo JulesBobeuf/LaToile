6339cdcf4bc04199702cf6f298ec247aaca45601
AymericJak <aymericjakpro@gmail.com>
2022-12-14 16:40:39 +0100

    Fix nom auteur

--- resources/views/oeuvres/index.blade.php
+++ resources/views/oeuvres/index.blade.php
@@ -12,7 +12,7 @@
         <ul>
             @foreach($oeuvres as $oeuvre)
                 <li> Nom : {{$oeuvre['nom']}} <br> media_url : {{$oeuvre['media_url']}} <br> thumbnail_url : {{$oeuvre['thumbnail_url']}} <br> description : {{$oeuvre['description']}}
-                    <br> coord_x : {{$oeuvre['coord_x']}} <br> coord_y : {{$oeuvre['coord_y']}} <br> salle_id : {{$oeuvre['salle_id']}} <br> auteur_id : {{$oeuvre['auteur_id']}}
+                    <br> coord_x : {{$oeuvre['coord_x']}} <br> coord_y : {{$oeuvre['coord_y']}} <br> salle_id : {{$oeuvre['salle_id']}} <br> auteur : {{$oeuvre['auteur']}}
                     <br> date_creation : {{$oeuvre['date_creation']}}  <br> style : {{$oeuvre['style']}}  <br> valide : {{$oeuvre['valide']}}  <br> <a href="{{route('oeuvres.show',$oeuvre->id)}}">Montrer cette oeuvre</a> <br>
                 <br><hr><br>
             @endforeach
