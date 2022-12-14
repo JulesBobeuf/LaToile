679b34e8ae3314591d1500f8c7a378a906d31791
Thomas Santoro <thomas_santoro@ens.univ-artois.fr>
2022-12-14 13:01:09 +0100

    Creation de l'index de salle

--- resources/views/salles/index.blade.php
+++ resources/views/salles/index.blade.php
@@ -1,1 +1,26 @@
-<?php
+@extends('layout.app')
+
+@section('title', 'Liste de salles')
+
+@section('content')
+    <h2 class="title">La liste des salles</h2>
+
+    <h4 class="title is-4">Filtrage par catégorie</h4>
+
+    <form action="{{route('salles.index')}}" method="get">
+        <input type="submit" value="OK">
+    </form>
+
+    @if(!empty($salles))
+        <ul>
+            @foreach($salles as $salle)
+                <li>Theme : {{$salle['theme']}}</li>
+                <a href="{{route('salles.show',$salle->id)}}">
+                    <button class="button">Afficher ce commentaire</button>
+                </a>
+            @endforeach
+        </ul>
+    @else
+        <h3>aucune salle</h3>
+    @endif
+@endsection
