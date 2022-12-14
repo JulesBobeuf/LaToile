b122da3b1059f9d089bd99bbdc961f705e192b4a
AymericJak <aymericjakpro@gmail.com>
2022-12-14 16:38:17 +0100

    MAJ commentaire policy

--- app/Policies/CommentairePolicy.php
+++ app/Policies/CommentairePolicy.php
@@ -11,11 +11,11 @@ class CommentairePolicy {
     use HandlesAuthorization;
 
     function update(User $user, Commentaire $commentaire) {
-        return Auth::user()->id === $commentaire->idVisiteur;
+        return Auth::user()->id === $commentaire->user_id;
     }
 
     function delete(User $user, Commentaire $commentaire) {
-        return $user->admin || Auth::user()->id === $commentaire->idVisiteur;
+        return $user->admin || Auth::user()->id === $commentaire->user_id;
     }
 
     function create(User $user) {
