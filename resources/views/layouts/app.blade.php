9c1c3e30c04fedf90f4b37c0f707e84670371396
AymericJak <aymericjakpro@gmail.com>
2022-12-14 11:29:28 +0100

    Division de la vue app.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,30 +14,11 @@
     @vite(['resources/scss/app.scss','resources/css/app.css','resources/js/app.js'])
 </head>
 <body>
-<nav>
-    <ul>
-        @guest
-            <li><a href="{{ route('login') }}">Login</a></li>
-            <li><a href="{{ route('register') }}">Register</a></li>
-        @else
-            <li> Bonjour {{ Auth::user()->name }}</li>
-            @if (Auth::user())
-                <li><a href="#">Des liens spécifiques pour utilisateurs connectés..</a></li>
-            @endif
-            <li><a href="{{ route('logout') }}" onclick="event.preventDefault(); document.
-          getElementById('logout-form').submit();">
-                    Logout
-                </a></li>
-            <form id="logout-form" action="{{ route('logout') }}"
-                  method="POST" style="display: none;"> {{ csrf_field() }}
-            </form>
-        @endguest
-    </ul>
-</nav>
-
-
+@include('layouts.header')
 
 @yield('content')
+
+@include('layouts.footer')
 
 </body>
 </html>
