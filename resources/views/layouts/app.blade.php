a0756315348c0056532298caedbc203f3e595565
Jules B <bobeuf.jules@gmail.com>
2022-12-15 08:35:57 +0100

    ajout darkmode

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,10 @@
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
+    <script type="text/javascript" src="script.js" defer></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/darkmode-js@1.5.7/lib/darkmode-js.min.js"></script>
+
     <!-- Scripts -->
     @vite(['resources/scss/app.scss','resources/css/app.css','resources/js/app.js'])
 </head>
